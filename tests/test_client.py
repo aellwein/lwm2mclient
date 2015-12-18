4f7a813f94b09d21b8ec2d0839a46a980bf42430
Alex Ellwein <alex.ellwein@gmail.com>
2015-12-18 20:20:16 +0100

    add more tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,5 +5,20 @@
 #
 # lwm2mclient is free software, this file is published under the MIT license as
 # described in the accompanying LICENSE file.
+import pytest
 
 from client import *
+
+
+@pytest.fixture
+def model():
+    return ClientModel()
+
+
+@pytest.fixture
+def client(model):
+    return Client(model)
+
+
+def test_bla(client, model):
+    assert client.model == model
