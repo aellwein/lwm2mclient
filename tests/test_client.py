744b0e746e96ab1c3472408c422ce472a6c7cd4c
Alex Ellwein <alex.ellwein@gmail.com>
2015-12-18 17:58:31 +0100

    create test scripts for client and encdec

---
+++ tests/test_client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 Alexander Ellwein
+#
+# lwm2mclient is free software, this file is published under the MIT license as
+# described in the accompanying LICENSE file.
+
+from client import *
