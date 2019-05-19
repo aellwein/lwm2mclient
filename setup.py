2eea0923466cbc6521595823f292fcea157e8562
Alex Ellwein <alex.ellwein@gmail.com>
2019-05-19 13:46:22 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-
 from setuptools import setup
 
 setup(
-    name="lwm2mclient",
-    version="0.1.0+git",
-    description="Lightweight M2M Client written in Python",
-    author="Alexander Ellwein",
-    author_email="alex.ellwein@gmail.com",
-    license="MIT License",
-    install_requires=["aiocoap>=0.2", "hexdump"]
+    name='lwm2mclient',
+    version='0.1.0+git',
+    description='Lightweight M2M Client written in Python',
+    author='Alexander Ellwein',
+    author_email='alex.ellwein@gmail.com',
+    license='MIT License',
+    install_requires=[
+        'aiocoap==0.4a1',
+        'hexdump==3.3'
+    ]
 )
