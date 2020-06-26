958977e0eb036d18d7a3eab80b7f838b469a69ee
Alex Ellwein <alex.ellwein@gmail.com>
2020-06-26 18:37:36 +0200

    default port should be random for client

--- lwm2m/client/defaults.py
+++ lwm2m/client/defaults.py
@@ -8,7 +8,7 @@ class Defaults(enum.Enum):
     SERVER_HOST = '::1'
     SERVER_PORT = COAP_PORT
     CLIENT_HOST = '::1'
-    CLIENT_PORT = 56830
+    CLIENT_PORT = 0  # 0 means using a random port
     LOGLEVEL = 'WARN'
     MODEL_LOADER = 'lwm2m.client.model:YamlModelLoader'
     MODEL_PATH = os.path.join(os.path.dirname(__file__), 'data')
